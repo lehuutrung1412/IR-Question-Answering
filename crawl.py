7311f67463091bf159efe35f5cb453f1cb518a9d
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-07-19 15:31:40 +0700

    speedup get content

--- crawl.py
+++ crawl.py
@@ -9,39 +9,37 @@ class Documents:
     def __init__(self, query):
         self.query = query
         self.link_search = search(self.query)
-        
-    def get_(self):
-        docs = []
-        count = 0
-        i = 0
-        while count < 5 and i < 10:
-            url = self.link_search[i]
-            if url.split('/')[0] == '':
-                i += 1
+
+    def gett(self, link_search):
+        url = link_search
+        if url.split('/')[0] == '':
+            return ''
+        html = requests.get(url, timeout = 4)
+        tree = BeautifulSoup(html.text,'lxml')
+        para = tree.findAll(['p', 'span', 'h1', 'h2', 'h3', 'li'])
+        doc = []
+        for p in para:
+            p_text = p.getText()
+            if p_text == '\n':
                 continue
-            html = requests.get(url, timeout = 4)
-            tree = BeautifulSoup(html.text,'lxml')
-            para = tree.findAll(['p', 'span', 'h1', 'h2', 'h3', 'li'])
-            doc = []
-            for p in para:
-                p_text = p.getText()
-                if p_text == '\n':
-                    continue
-                doc.append(p_text)
-            doc_str = ' '.join(doc)
-            doc_str = re.sub(r'\[.*?\]', '', doc_str)
-            # doc_str = re.sub('\n\n', '\n', doc_str)
-            doc_str = re.sub('\xa0', ' ', doc_str)
-            if doc_str == '':
-                i += 1
-                continue
-            docs.append(doc_str)
-            i += 1
-            count += 1
-        return docs
+            doc.append(p_text)
+        doc_str = ' '.join(doc)
+        doc_str = re.sub(r'\[.*?\]', '', doc_str)
+        # doc_str = re.sub('\n\n', '\n', doc_str)
+        doc_str = re.sub('\xa0', ' ', doc_str)
+        return doc_str
+
 
     def get(self):
         threads = min(MAX_THREADS, len(self.link_search))
+        docs = []
+        count = 0
         with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-            docs = [executor.submit(self.get_, self.link_search).result()]
+            for link_search in self.link_search:
+                if count > 4:
+                    break
+                doc = executor.submit(self.gett, link_search).result()
+                if doc != '':
+                    docs.append(doc)
+                    count += 1
         return docs
