5c33961b4c907f9238987ad5ecfe543cdd82e10d
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-07-19 09:50:59 +0700

    Add script file

---
+++ app/static/js/script.js
@@ -0,0 +1,45 @@
+const question_input = document.getElementsByClassName("question-input")[0];
+const answer = document.getElementsByClassName("ans")[0];
+const textans = document.getElementsByClassName("textans")[0];
+
+question_input.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        console.log(question_input.value);
+        if (question_input.value === ''){
+            alert('Vui lòng nhập câu hỏi!');
+        }
+        else{
+            upload();
+        }
+    }
+});
+
+function upload(){
+    let fileForm = new FormData();
+    if (question_input.value){
+        fileForm.append('question', question_input.value);
+    }
+    $.ajax({
+        method: 'POST',
+        url: '/send',
+        data: fileForm,
+        processData: false,
+        cache: false,
+        contentType: false,
+        success: function (response){
+            if (response){
+                console.log(response);
+                let ans = response['ans'];
+                let text = response['text'];
+                answer.value = ans;
+                textans.value = text;
+            }
+            else{
+                console.log('No response');
+            }
+        },
+        error: function(error){
+            console.log(error);
+        }
+    });
+}
