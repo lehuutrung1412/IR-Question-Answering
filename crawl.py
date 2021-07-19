103b0716bca8f3677e6a89bde4a9a184ae2458a6
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-07-19 21:31:44 +0700

    Update crawl faster

--- crawl.py
+++ crawl.py
@@ -20,17 +20,20 @@ class Documents:
             return ''
         html = self.requests_session.get(url, timeout = 4)
         tree = BeautifulSoup(html.text,'lxml')
-        para = tree.findAll(['p', 'span', 'h1', 'h2', 'h3', 'li'])
+        para = tree.findAll(['p'])
         doc = []
         for p in para:
             p_text = p.getText()
             if p_text == '\n':
                 continue
+            if len(p_text) < 30:
+                continue
             doc.append(p_text)
         doc_str = ' '.join(doc)
         doc_str = re.sub(r'\[.*?\]', '', doc_str)
-        # doc_str = re.sub('\n\n', '\n', doc_str)
         doc_str = re.sub('\xa0', ' ', doc_str)
+        doc_str = re.sub(' +', ' ', doc_str)
+        doc_str = re.sub('\n\n', '\n', doc_str)
         return doc_str
 
 
