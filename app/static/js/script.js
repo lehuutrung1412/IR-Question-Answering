14bbc4085fb5890840155a5ac23e08b82032559b
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-07-20 22:19:40 +0700

    Update frontend

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,10 +1,13 @@
 const question_input = document.getElementsByClassName("question-input")[0];
 const answer = document.getElementsByClassName("ans")[0];
 const textans = document.getElementsByClassName("textans")[0];
+const answer_ = document.getElementsByClassName("answer")[0];
 
 question_input.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
         console.log(question_input.value);
+        answer_.style.visibility = "hidden";
+        textans.style.visibility = "hidden";
         if (question_input.value === ''){
             alert('Vui lòng nhập câu hỏi!');
         }
@@ -36,6 +39,8 @@ function upload(){
                 text = text.slice(0, start) + "<strong>" + text.slice(start, end) + "</strong>" + text.slice(end, text.lenght);
                 answer.value = ans;
                 textans.innerHTML = text;
+                answer_.style.visibility = "visible";
+                textans.style.visibility = "visible";
             }
             else{
                 console.log('No response');
