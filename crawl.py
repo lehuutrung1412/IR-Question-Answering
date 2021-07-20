559462fb2a190a718c8c2277614a7a8959f18c02
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-07-20 21:53:05 +0700

    Faster verson

--- crawl.py
+++ crawl.py
@@ -1,52 +1,70 @@
-from googlesearch import search
-from bs4 import BeautifulSoup
-import requests
-import re
-import concurrent.futures
-import cchardet
-import lxml
+# from googlesearch import search
+# from bs4 import BeautifulSoup
+# import requests
+# import re
+# import concurrent.futures
+# import cchardet
+# import lxml
 
-MAX_THREADS = 30
+# MAX_THREADS = 30
 
-class Documents:
-    def __init__(self, query):
-        self.query = query
-        self.link_search = search(self.query)
-        self.requests_session = requests.Session()
+# class Documents:
+#     def __init__(self, query):
+#         self.query = query
+#         self.link_search = search(self.query)
+#         self.requests_session = requests.Session()
 
-    def gett(self, link_search):
-        url = link_search
-        if url.split('/')[0] == '':
-            return ''
-        html = self.requests_session.get(url, timeout = 4)
-        tree = BeautifulSoup(html.text,'lxml')
-        para = tree.findAll(['p'])
-        doc = []
-        for p in para:
-            p_text = p.getText()
-            if p_text == '\n':
-                continue
-            if len(p_text) < 30:
-                continue
-            doc.append(p_text)
-        doc_str = ' '.join(doc)
-        doc_str = re.sub(r'\[.*?\]', '', doc_str)
-        doc_str = re.sub('\xa0', ' ', doc_str)
-        doc_str = re.sub(' +', ' ', doc_str)
-        doc_str = re.sub('\n\n', '\n', doc_str)
-        return doc_str
+#     def gett(self, link_search):
+#         url = link_search
+#         if url.split('/')[0] == '':
+#             return ''
+#         html = self.requests_session.get(url, timeout = 4)
+#         tree = BeautifulSoup(html.text,'lxml')
+#         para = tree.findAll(['p'])
+#         doc = []
+#         for p in para:
+#             p_text = p.getText()
+#             if p_text == '\n':
+#                 continue
+#             if len(p_text) < 30:
+#                 continue
+#             doc.append(p_text)
+#         doc_str = ' '.join(doc)
+#         doc_str = re.sub(r'\[.*?\]', '', doc_str)
+#         doc_str = re.sub('\xa0', ' ', doc_str)
+#         doc_str = re.sub(' +', ' ', doc_str)
+#         doc_str = re.sub('\n\n', '\n', doc_str)
+#         return doc_str
 
 
-    def get(self):
-        threads = min(MAX_THREADS, len(self.link_search))
-        docs = []
-        count = 0
-        with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-            for link_search in self.link_search:
-                if count > 4:
-                    break
-                doc = executor.submit(self.gett, link_search).result()
-                if doc != '':
-                    docs.append(doc)
-                    count += 1
-        return docs
+#     def get(self):
+#         threads = min(MAX_THREADS, len(self.link_search))
+#         docs = []
+#         count = 0
+#         with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
+#             for link_search in self.link_search:
+#                 if count > 4:
+#                     break
+#                 doc = executor.submit(self.gett, link_search).result()
+#                 if doc != '':
+#                     docs.append(doc)
+#                     count += 1
+#         return docs
+
+from googleapiclient.discovery import build
+
+class GoogleSearchAPI:
+    def __init__(self):
+        self.api_key = 'AIzaSyDKZLvtz9C5tPbH517yF0jLRm3rPFFH3ik'
+        self.custom_search_key = '7e978a8ce9b33aae4'
+    
+    def search(self, query, **kwargs):
+         query_service = build("customsearch", "v1", developerKey=self.api_key)
+         query_results = query_service.cse().list(q=query, cx=self.custom_search_key, **kwargs).execute()
+         return query_results['items']
+
+    def get_link(self, query, num=5):
+        return [result['link'] for result in self.search(query, num=5)]
+    
+    def get_passages(self, query, num=5):
+        return [item_['snippet'] for item_ in self.search(query, num)]
--- main.py
+++ main.py
@@ -1,15 +1,13 @@
-from crawl import Documents
-from retrieval import PassageRetrieval
+from crawl import GoogleSearchAPI
+# from retrieval import PassageRetrieval
 from extract import Reader
 
 model_name = 'bert-large-uncased-whole-word-masking-finetuned-squad'
-passage_retriever = PassageRetrieval()
+# passage_retriever = PassageRetrieval()
 reader = Reader(model_name)
 
 def main(question):
-    docs = Documents(question).get()
-    passage_retriever.fit(docs)
-    passages = passage_retriever.most_similar(question)
+    passages = GoogleSearchAPI().get_passages(question)
     answers = reader.extract(question, passages)
     return answers[0]
     
