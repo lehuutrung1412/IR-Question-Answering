08d0cc612ee9ce456e3fd6ff0ce893baaedf89ac
lehuutrung1412 <lehuutrung1412@users.noreply.github.com>
2021-07-21 17:36:35 +0700

    Update stable frontend

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,10 +1,13 @@
 const question_input = document.getElementsByClassName("question-input")[0];
 const answer = document.getElementsByClassName("ans")[0];
 const textans = document.getElementsByClassName("textans")[0];
+const answer_ = document.getElementsByClassName("answer")[0];
 
 question_input.addEventListener("keyup", function(event) {
     if (event.key === "Enter") {
         console.log(question_input.value);
+        answer_.style.visibility = "hidden";
+        textans.style.visibility = "hidden";
         if (question_input.value === ''){
             alert('Vui lòng nhập câu hỏi!');
         }
@@ -29,10 +32,15 @@ function upload(){
         success: function (response){
             if (response){
                 console.log(response);
-                let ans = response['ans'];
+                let ans = response['answer'];
                 let text = response['text'];
+                let start = response['start'];
+                let end = response['end'];
+                text = text.slice(0, start) + "<strong>" + text.slice(start, end) + "</strong>" + text.slice(end, text.lenght);
                 answer.value = ans;
-                textans.value = text;
+                textans.innerHTML = text;
+                answer_.style.visibility = "visible";
+                textans.style.visibility = "visible";
             }
             else{
                 console.log('No response');
